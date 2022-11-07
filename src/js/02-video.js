6b6b8233fe194810e4ae2ba1b13d8a915dbff88d
yurii-kovalchuk <sogdrums@gmail.com>
2022-11-07 16:08:14 +0200

    done 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeRef = document.querySelector('#vimeo-player');
+const player = new Player(iframeRef);
+
+player.on(
+  'timeupdate',
+  throttle(
+    data => localStorage.setItem('videoplayer-current-time', data.seconds),
+    1000
+  )
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+// player
+//   .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
